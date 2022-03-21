f500e187f78ca148979b56335cce6644dc58761b
diksha khandelwal <diksha.khandelwal@daffodilsw.com>
2022-03-21 15:55:09 +0530

    update cart

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface records {
+  rating : Array<number>,
+  feedback : Array<string>,
+  _id : string,
+  shopName : string,
+  shopContactNumber: string,
+    openTime: string,
+    closeTime: string,
+    address: string,
+    state: string,
+    city: string,
+    postalCode: string,
+    landmark: string,
+    discountPer: number,
+    ownerName: string,
+    ownerEmail: string,
+    registeredDate: string,
+    status: boolean,
+
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  @Input() data : any 
+
+  @Input() data : any ;
+
   
-  dummyData : any = {
-    rating: [1],
+ dummyData : records = {
+    rating: [],
     feedback: [],
     _id: '607537ac0f87c626c9b3641c',
     shopName: 'Shop 1',
