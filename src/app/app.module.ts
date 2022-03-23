c3cbb2e222b1ba19ec470b3649e51d971778bfee
diksha khandelwal <diksha.khandelwal@daffodilsw.com>
2022-03-23 10:10:59 +0530

    resolve conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { CardscreenComponent } from './cardscreen/cardscreen.component';
-// import {MatButtonModule} from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
-import { CardComponent } from './card/card.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardComponent } from './card/card.component';
 import { MatIconModule } from '@angular/material/icon';
 import { AuthModule } from './auth/auth.module';
 import { MaterialModule } from './material.module';
@@ -18,10 +18,11 @@ import { CoreModule } from './core/core.module';
 import AuthComponent from './auth/auth.component';
 
 @NgModule({
-  declarations: [AppComponent, CardscreenComponent,AuthComponent],
-
-
-
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardscreenComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
