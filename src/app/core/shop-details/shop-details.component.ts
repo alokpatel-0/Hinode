a2cee73af4659130463c6e1e908a860da0bbc306
ramkumardaffodil <ram.kumar@daffodilsw.com>
2022-03-24 17:05:36 +0530

    integrated api

--- src/app/core/shop-details/shop-details.component.ts
+++ src/app/core/shop-details/shop-details.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ICategory, IShopDetail } from 'src/app/shared/models/shopDetails';
+import { ShopDetailsService } from 'src/app/shared/services/shop-details.service';
 
 @Component({
   selector: 'app-shop-details',
   templateUrl: './shop-details.component.html',
-  styleUrls: ['./shop-details.component.scss']
+  styleUrls: ['./shop-details.component.scss'],
 })
 export class ShopDetailsComponent implements OnInit {
+  allShopData!: Array<IShopDetail>;
+  availableCatergory!: Array<ICategory>;
+  shopDataWithCategory!: Array<IShopDetail>;
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private shopDetailService: ShopDetailsService
+  ) {}
 
   ngOnInit(): void {
+    this.fetchCategories();
+    this.fetchDataWithParams();
   }
 
+  fetchCategories() {
+    this.shopDetailService.getAvailableCategory().subscribe((response: any) => {
+      this.availableCatergory = response.data;
+    });
+  }
+
+  fetchDataWithParams() {
+    this.activateRoute.params.subscribe((queryParamsResponse: any) => {
+      if (queryParamsResponse.id) {
+        this.shopDetailService
+          .getShopDataWithId(queryParamsResponse.id)
+          .subscribe((response: any) => {
+            this.allShopData = response.data;
+            this.availableCatergory = this.availableCatergory.filter(
+              (availCat: any) =>
+                this.allShopData.filter(
+                  (shopData: any) => availCat._id === shopData.categoryName
+                )[0]
+            );
+            this.handleCategoryClick(response.data[0].categoryName, 0);
+          });
+      }
+    });
+  }
+
+  handleCategoryClick(id: string, i: number) {
+    this.shopDataWithCategory = this.allShopData.filter((data: any) => {
+      if (data.categoryName === id) {
+        data.populateCategoryName = this.availableCatergory[i].category;
+        return data;
+      }
+    });
+  }
 }
---
+++ src/app/shared/models/shopDetails.ts
@@ -0,0 +1,28 @@
+export interface ICategory {
+  _id: string;
+  category: string;
+}
+
+export interface IQuantity {
+  availablein: string;
+  discount: number;
+  inStockNumber: number;
+  productId: string;
+  productprice: number;
+  _id: string;
+}
+
+export interface IShopDetail {
+  brandName: string;
+  categoryName: string;
+  populateCategoryName?: string;
+  feedback: Array<any>;
+  imageUrl: Array<string>;
+  ingredients: string;
+  keyFeatures: string;
+  productName: string;
+  quantitySet: Array<IQuantity>;
+  rating: Array<any>;
+  status: string;
+  _id: string;
+}
