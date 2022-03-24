b1e0a69e8b34518d6a05f4025382fa1f62696eeb
takshdaffodil <taksh@daffodilsw.com>
2022-03-24 10:02:35 +0530

    created auth guard for protected routes

---
+++ src/app/shared/guards/protected-route.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProtectedRouteGuard implements CanActivate {
+  constructor(public router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (localStorage.getItem('user') === null) {
+      this.router.navigate(['shop']);
+      return false;
+    }
+    return true;
+  }
+}
